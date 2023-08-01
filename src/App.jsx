c1e68cfebfcf6a7a676170ee55b99a4738666c06
Aakanksha <tasaiaakanksha851@gmail.comgit config --global user.email tasaiaakanksha851@gmail.com>
2023-08-01 12:56:22 +0530

    web2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home from './screens/Home'
+import User from './components/User'
+
 
 const App = () => {
   return (
 <>
+{/* <User/> */}
 <Home/>
+
 </>
   )
 }
---
+++ src/components/User.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div className='w-full h-screen bg-black flex items-center justify-center '>
+        <div className='w-[70%] h-[70%]   flex items-center justify-center flex-col'>
+            <div className='w-[60%] h-[20%] flex items-center justify-center  '>
+                <span className='text-[40px] font-bold text-white'>who is watching?</span>
+
+            </div>
+            <div className='w-[80%] h-[40%] flex items-center justify-evenly flex-row '>
+                
+            <div className='w-[12%] h-[45%] bg-blue-600'>
+                
+                
+                
+            </div>
+            <div className='w-[12%] h-[45%] bg-slate-400'></div>
+            <a href='https://netflix2-theta.vercel.app/' className='w-[12%] h-[45%] bg-yellow-400'></a>
+            <div className='w-[12%] h-[45%] bg-slate-600'></div>
+            <div className='w-[12%] h-[45%] bg-red-600'></div>
+
+
+
+
+
+            </div>
+            
+            
+        </div>
+    </div>
+  )
+}
+
+export default User
